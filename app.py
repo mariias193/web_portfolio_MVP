f63a802985ffd4f5e0146af225375deffcdfbcec
mariias193 <skovermsh@gmail.com>
2024-07-17 13:27:42 +0300

    Update app.py changes

--- app.py
+++ app.py
@@ -85,12 +85,10 @@ def web_portfolio():
     st.subheader("About Me")
 
     st.markdown("""
-    - 🧑‍💻🚀 I am a **Data Scientist and Gen AI developer** at [Digna AS](https://www.Digna.no/), 
-    where I am currently working on an AI platform to help simplify public information to newcomers to Norway.
-    - ❤️ I am passionate about *Machine Learning/Deep Learning, Artifical intelligence, Data Science, Software Engineering, 
-   Data Analytics, Automation*, and more!
-    - 🏂 In my free time, I enjoy practising sports such as weight lifting and chalistenics.
-    - 🪧 You can reach me at said@diga.no.
+    - I study at the Odessa Law Academy in Ukraine.
+    - ❤️ I am passionate about reading and learning new things and I am currently learning Norwegian.
+    - 🏂 In my free time, I enjoy to do yoga, spend time with friends or just sleep.
+    - I also have experience working as a waiter.
     - 🏠 Based in Norway.
     """)
 
